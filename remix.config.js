2a0362583efa189aa096152edd8c82b71d5da6a4
Sarah-88 <sarahlim88@hotmail.com>
2023-10-30 00:55:13 +0800

    Remove netlify

--- remix.config.js
+++ remix.config.js
@@ -1,15 +1,5 @@
-const baseConfig =
-    process.env.NODE_ENV === "production"
-        ? // when running the Netify CLI or building on Netlify, we want to use
-        {
-            server: "./server.ts",
-            serverBuildPath: ".netlify/functions-internal/server.js",
-        }
-        : // otherwise support running remix dev, i.e. no custom server
-        undefined;
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-    ...baseConfig,
     ignoredRouteFiles: ["**/.*"],
     // This works out of the box with the Netlify adapter, but you can
     // add your own custom config here if you want to.
